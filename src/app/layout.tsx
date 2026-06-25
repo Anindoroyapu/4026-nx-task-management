b76791d496095a48bedf6dc042f58c7b0089f788
Anindo Roy <142706157+Anindoroyapu@users.noreply.github.com>
2026-06-25 13:05:49 +0600

    update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,16 +14,18 @@ export default function RootLayout({
     <html lang="en">
       <head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin="anonymous"
+        />
         <link
           href="https://fonts.googleapis.com/css2?family=Manrope:wght@400;500;600;700;800&family=Playfair+Display:ital,wght@1,600;1,700&display=swap"
           rel="stylesheet"
         />
       </head>
       <body>
-        <div className="page-shell">
-          {children}
-        </div>
+        <div className="page-shell">{children}</div>
       </body>
     </html>
   );
